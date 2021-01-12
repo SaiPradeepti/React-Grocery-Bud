08f7a8ce24248ba21a0e0366b882a02087986d66
SaiPradeepti <saipradeepti14@gmail.com>
2021-01-12 21:44:56 +0530

    Add Items

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ function App() {
     });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    if(!name){}
+    else if(name && isEditing){}
+    else{
+      const newItem = {id: new Date().getTime().toString(),title: name};
+      setList([...list,newItem]);
+      setName('');
+    }
   }
   return <section className="section-center">
     <form className="grocery-form" onSubmit={handleSubmit}>
@@ -27,10 +33,12 @@ function App() {
         </button>
       </div>
     </form>
-    <div className="grocery-container">
-      <List />
+    {list.length > 0 && (
+      <div className="grocery-container">
+      <List items={list}/>
       <button className="clear-btn">clear items</button>
     </div>
+    )}
   </section>
 }
 
